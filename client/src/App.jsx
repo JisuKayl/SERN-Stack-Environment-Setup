9c877f2c34eeacf74ce194740e86ff912a848245
Jerson Kyle Bonifacio <143814993+JisuKayl@users.noreply.github.com>
2025-02-27 19:10:50 +0800

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/users")
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Users</h2>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.username}</td>
+              <td>{user.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const mysql = require("mysql2");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "mysql",
+  database: "sernstack_db",
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to MySQL Database!");
+});
+
+app.get("/users", (req, res) => {
+  db.query("Select * FROM users", (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: "Database error" });
+    }
+    res.status(200).json(results);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
